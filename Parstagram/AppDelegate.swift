3f9ac25d70969454c1b77f1e7177fc571ea4888a
cyrejorin <cyrejorin@gmail.com>
2020-02-23 13:11:39 -0800

    Commit1

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -9,7 +9,7 @@ import Parse
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                        configuration.server = "https://warm-bastion-63696.herokuapp.com/parse"
                    })
         )
+        
         return true
     }
 
